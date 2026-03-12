7a1ec57536dd2d42958836a407b4027fe3046497
DUCROT FREDERIC <frederic.ducrot@hotmail.com>
2026-03-12 13:21:53 +0000

    save

---
+++ includes/cub3d.h
@@ -0,0 +1,7 @@
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#include "cub3d_struct.h"
+
+#endif 
---
+++ includes/cub3d_struct.h
@@ -0,0 +1,10 @@
+#ifndef CUB3D_STRUCT_H
+# define CUB3D_STRUCT_H
+
+typedef struct s_data	
+{
+	
+}
+t_data;
+
+#endif // CUB3D_STRUCT_H
