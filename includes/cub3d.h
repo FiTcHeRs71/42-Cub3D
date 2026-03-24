2b0fb77aad9fca0d5664ba0403e11471d3b8221c
DUCROT FREDERIC <frederic.ducrot@hotmail.com>
2026-03-24 11:44:56 +0000

    clear leaks OK git add .! invalid read size repare

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -13,8 +13,8 @@
 # define MALLOC_FAILED "Memory allocation failed, exit \n"
 # define FD_ERROR "Unable to open map file, exit \n"
 # define INVALID_MAP "Invalid map, exit \n"
-# define INVALID_SETTINGS "Invalid configuration format or missing elements\n"
-# define NB_PLAYER "Map must contain only one player\n"
+# define INVALID_SETTINGS "Invalid configuration format or missing elements, exit\n"
+# define NB_PLAYER "Map must contain only one player, exit\n"
 
 /*================== FONCTION ==================*/
 
