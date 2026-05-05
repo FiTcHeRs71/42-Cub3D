ad26983f4e5a2946534b16a18e8e02f36d85d9a4
Frederic DUCROT <frederic.ducrot@hotmail.com>
2026-05-05 18:25:39 +0200

    save

--- includes/fred.h
+++ includes/fred.h
@@ -3,8 +3,4 @@
 
 # include "cub3d_struct.h"
 
-void	init_window(t_data *data, t_mlx *mlx);
-int	handle_keyboard_input(int keycode, t_mlx *mlx);
-int	close_window(t_mlx *mlx, t_data *data);
-
 #endif
